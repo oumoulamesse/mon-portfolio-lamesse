8d12fd2c5c9452c2917440a0586cbff6309f7dc3
oumoulamesse <oumoulamesse@gmail.com>
2025-03-10 16:30:41 +0100

    first commit portfolio

---
+++ src/app/home/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import AOS from 'aos';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
